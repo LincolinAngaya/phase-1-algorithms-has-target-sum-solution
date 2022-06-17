85d229775bf94465749ba035c41c4d1ee8a59cf1
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-06-17 13:59:47 +0300

    commit tree 1fa8ceb8bd46d211ff75533952498a6ae7f37f75

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+    // Write your algorithm here
+    for (let x = 0; x < array.length; x++) {
+        const complement = target - array[x];
+        for (let y = x + 1; y < array.length; y++) {
+            if (array[y] === complement) return true;
+
+        }
+
+    }
+    return false;
 }
 
 /* 
@@ -8,6 +17,15 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  iterate over the remaining numbers in the array
+    check if any of the remaining numbers is the complement
+      if so, return true
+if we reach the end of the array, return false
+time complexity of O(n)
+space complexity of O(n)
 */
 
 /*
@@ -16,19 +34,19 @@ function hasTargetSum(array, target) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+    console.log("Expecting: false");
+    console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
